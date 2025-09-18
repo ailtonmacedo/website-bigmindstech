b7ebd2fecb54881485f80a3ee583ee1add24e196
Ailton Macedo <ailtonwsb@gmail.com>
2025-09-18 02:05:30 +0000

    A logomarca deve estar por cima da imagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Logo } from '@/components/ui/logo';
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
-      <header className="absolute top-0 left-0 p-8">
+      <header className="absolute top-0 left-0 p-8 z-20">
         <Logo />
       </header>
       <main className="flex-grow flex items-center justify-center">
